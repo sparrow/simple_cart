ec4440891205c0418e18d81f292bd13c644d0614
Vladimir Vorobyov <sparrowpublic@gmail.com>
2012-08-28 19:01:04 +0300

    Add to config current_shopper_method, on_checkout_method, on_failed_method, on_success_method, on_expire_method

--- lib/simple_cart.rb
+++ lib/simple_cart.rb
@@ -2,22 +2,22 @@ require 'rails'
 
 module SimpleCart
 
-  mattr_reader :current_shopper_method
+  mattr_accessor :current_shopper_method
   @@current_shopper_method = :current_user
 
   mattr_accessor :cartables
   @@cartables = []
 
-  mattr_reader :on_checkout_method
+  mattr_accessor :on_checkout_method
   @@on_checkout_method = nil
 
-  mattr_reader :on_failed_method
+  mattr_accessor :on_failed_method
   @@on_failed_method = nil
 
-  mattr_reader :on_success_method
+  mattr_accessor :on_success_method
   @@on_success_method = nil
 
-  mattr_reader :on_expire_method
+  mattr_accessor :on_expire_method
   @@on_expire_method = nil
 
   def self.setup
